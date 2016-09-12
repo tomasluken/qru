5118391e889b226217dff80366d4471452d08203
tomasluken <tomas.luken@gmail.com>
2016-09-12 11:22:26 +0200

    Scan QR (and read result) via "Barcode Scanner"

--- app/src/main/java/nl/tomasluken/helloworld3/MainActivity.java
+++ app/src/main/java/nl/tomasluken/helloworld3/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.zxing.integration.android.*;
+
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "nl.tomasluken.helloworld3.MESSAGE";
     @Override
@@ -22,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void startScan(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        if (scanResult != null) {
+            editText.setText(scanResult.getContents());
+        }
+        else {
+            // else continue with any other code you need in the method
+            editText.setText("scanresult was null...");
+        }
     }
 }
