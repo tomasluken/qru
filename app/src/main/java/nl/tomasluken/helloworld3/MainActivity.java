4b7502bb6a27e1d54736feff017ab9fb597ad7da
tomasluken <tomas.luken@gmail.com>
2016-09-12 11:57:45 +0200

    finetune gui

--- app/src/main/java/nl/tomasluken/helloworld3/MainActivity.java
+++ app/src/main/java/nl/tomasluken/helloworld3/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.zxing.integration.android.*;
 
@@ -17,14 +18,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
+    /*public void sendMessage(View view) {
         MyAppCore core = new MyAppCore();
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = core.YourNameIs(editText.getText().toString());
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }*/
 
     public void startScan(View view) {
         IntentIntegrator integrator = new IntentIntegrator(this);
@@ -33,13 +34,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        TextView lblResult = (TextView) findViewById(R.id.scan_result);
         if (scanResult != null) {
-            editText.setText(scanResult.getContents());
+            lblResult.setText(scanResult.getContents());
         }
         else {
-            // else continue with any other code you need in the method
-            editText.setText("scanresult was null...");
+            lblResult.setText("scanresult was null...");
         }
     }
 }
