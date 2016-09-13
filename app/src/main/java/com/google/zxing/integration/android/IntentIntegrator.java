9c7ffbf1b622db2a34699716013759832a855e7e
tomasluken <tomas.luken@gmail.com>
2016-09-13 10:03:48 +0200

    portrait always

--- app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -419,6 +419,7 @@ public class IntentIntegrator {
                 String formatName = intent.getStringExtra("SCAN_RESULT_FORMAT");
                 byte[] rawBytes = intent.getByteArrayExtra("SCAN_RESULT_BYTES");
                 int intentOrientation = intent.getIntExtra("SCAN_RESULT_ORIENTATION", Integer.MIN_VALUE);
+                //int intentOrientation = intent.getIntExtra("SCAN_RESULT_ORIENTATION", 1);
                 Integer orientation = intentOrientation == Integer.MIN_VALUE ? null : intentOrientation;
                 String errorCorrectionLevel = intent.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
                 return new IntentResult(contents,
