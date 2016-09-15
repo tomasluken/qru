fef272ab15b8ea331803cd65725f76a95aef8b3b
tomasluken <tomas.luken@gmail.com>
2016-09-15 16:35:47 +0200

    app menu

--- app/src/main/java/nl/tomasluken/qru/MainActivity.java
+++ app/src/main/java/nl/tomasluken/qru/MainActivity.java
@@ -3,9 +3,14 @@ package nl.tomasluken.qru;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.*;
 
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
     }
 
     private boolean scanMessage = true;
@@ -63,5 +70,35 @@ public class MainActivity extends AppCompatActivity {
         {
             lblResult.setText(ex.getMessage());
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        TextView lblResult = (TextView) findViewById(R.id.scan_result);
+        try {
+            switch (item.getItemId()) {
+                case R.id.action_clear:
+                    lblResult.setText(null);
+                    break;
+                case R.id.action_settings:
+                    Toast.makeText(this, "Settings; not implemented yet.", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_help:
+                    Toast.makeText(this, "Help; not implemented yet.", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+                }
+        }
+        catch (Exception ex) {
+            lblResult.setText(ex.getMessage());
+        }
+        return true;
     }
 }
